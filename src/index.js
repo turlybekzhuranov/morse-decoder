a56b3077ced8ed1c61596f4076d03baf44a6e8be
Turlybek Zhuranov <turlybekk@gmail.com>
2020-10-18 12:03:13 +0600

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -39,6 +39,39 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let morseCodes = expr.match(/.{1,2}/g);
+    let morseCode = '';
+
+    for (let i=0; i<morseCodes.length; i++){
+        
+        if (i%5 === 0 && i !== 0 && morseCodes[i-1] != '**'){
+            morseCode += ' ';
+        }
+        if (morseCodes[i] == '10') {
+            morseCode += '.';
+        }
+        else if (morseCodes[i] == '11') {
+            morseCode += '-';
+        }
+        else if (morseCodes[i] == '**'){
+            morseCode += '*';
+        }
+    }
+
+    let words = (morseCode).split('*****');
+    let letters = words.map((w) => w.split(' '));
+    let decoded = [];
+
+    for(let i = 0; i < letters.length; i++){
+        decoded[i] = [];
+        for(let x = 0; x < letters[i].length; x++){
+            if(MORSE_TABLE[letters[i][x]]){
+                decoded[i].push( MORSE_TABLE[letters[i][x]] );
+            }
+        }
+    }
+
+    return decoded.map(arr => arr.join('')).join(' ');
 }
 
 module.exports = {
